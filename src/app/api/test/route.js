1b6ba912f5fb5f87ca833226e7baca47c9691ee1
Alikhan Bekirov <alikhanbekirov@gmail.com>
2025-05-07 23:32:44 +0300

    step 1

---
+++ src/app/api/test/route.js
@@ -0,0 +1,7 @@
+export async function GET() {
+    console.log('SERVER: SECRET_KEY =', process.env.SECRET_KEY);
+    console.log('SERVER: API_KEY =', process.env.API_KEY);
+    console.log('SERVER: NEXT_PUBLIC_SITE_NAME =', process.env.NEXT_PUBLIC_SITE_NAME);
+
+    return Response.json({ ok: true });
+}
